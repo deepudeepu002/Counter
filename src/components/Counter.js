6fd7377bb5e8920c35fae87c96318bd786c42f0e
deepudeepu002 <emaildeeputr@gmail.com>
2025-01-17 09:19:41 +0530

    Create Counter for the flow of useState

---
+++ src/components/Counter.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+const Counter = ()=>{
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+
+    return(
+        <div>
+            <h1>Counter: { count }</h1>
+            <button onClick={ increment } > Increase </button>
+            <button onClick={ decrement } > Decrease </button>
+        </div>
+    )
+}
+
+export default Counter;
