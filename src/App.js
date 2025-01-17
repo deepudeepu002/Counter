60e8979982259797836127a7d78e7275522476ad
deepudeepu002 <emaildeeputr@gmail.com>
2025-01-17 12:03:14 +0530

    Logo removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Counter from './components/Counter';
 import { Provider } from 'react-redux';
 import store from './app/store';
@@ -10,7 +9,6 @@ function App() {
     <Provider store={ store }>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <Count />
           <Counter />
         </header>
